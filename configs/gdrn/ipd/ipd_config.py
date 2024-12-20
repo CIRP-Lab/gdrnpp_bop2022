f1717df544992fa357286ed3a80da5f7a4765fcb
Your Name <you@example.com>
2024-12-20 03:29:44 +0000

    update attempt to add ipd dataset, still not working properly. Throwing error bc #objs in ref/ipd.py is >20. 20 is the #objs in ycbv, which is where I started this iteration from

--- configs/gdrn/ipd/ipd_config.py
+++ configs/gdrn/ipd/ipd_config.py
@@ -43,7 +43,7 @@ SOLVER = dict(
 )
 
 DATASETS = dict(
-    TRAIN=("ipd",),
+    TRAIN=("ipd_train_pbr",),
     TEST=("ipd_test",),
     DET_FILES_TEST=("datasets/BOP_DATASETS/icbin/test/test_bboxes/yolox_x_640_icbin_pbr_icbin_bop_test.json",),
     DET_TOPK_PER_OBJ=100,
--- core/gdrn_modeling/datasets/dataset_factory.py
+++ core/gdrn_modeling/datasets/dataset_factory.py
@@ -27,7 +27,7 @@ from core.gdrn_modeling.datasets import (
     itodd_pbr,
     itodd_bop_test,
     itodd_d2,
-    ipd_train,
+    ipd_pbr,
 )
 
 
@@ -59,7 +59,7 @@ _DSET_MOD_NAMES = [
     "itodd_pbr",
     "itodd_bop_test",
     "itodd_d2",
-    "ipd_train",
+    "ipd_pbr",
 ]
 
 logger = logging.getLogger(__name__)
